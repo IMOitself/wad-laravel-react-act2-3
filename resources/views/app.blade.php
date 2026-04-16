3002608b4f505af841aa2fea60c28905ed1f300a
IMO <180146096+IMOitself@users.noreply.github.com>
2026-04-16 11:23:44 +0800

    feat: add global default font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
@@ -27,6 +27,9 @@
 
             html.dark {
                 background-color: oklch(0.145 0 0);
+            }
+            body {
+                font-family: 'JetBrains Mono', monospace;
             }
         </style>
 
